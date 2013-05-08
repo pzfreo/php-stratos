de7ee1fbef7a66f73b05651b5df615966bcffc09
Paul Fremantle <pzfreo@github.com>
2013-05-08 10:31:19 +0100

    ipdated index

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+<TITLE>
+<?php 
+	echo "Date" . date('l jS \of F Y h:i:s A');
+?>
+</TITLE>
+</HEAD>
+<BODY>
+<H1>Sample PHP App running on Stratos2</H1>
+
+<br>
+This app is using 
+<?php 
+echo 'Current PHP version: ' . phpversion(); 
+?>
+
