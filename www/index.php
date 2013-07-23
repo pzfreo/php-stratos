40496b77936668b34fee456e761f29c98f2e9c97
Paul Fremantle <pzfreo@github.com>
2013-07-23 19:34:32 +0100

    update

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
 </TITLE>
 </HEAD>
 <BODY>
-<H1>Sample PHP App running on Stratos2</H1>
+<H1>Sample PHP App running on Stratos2 in Mountain View</H1>
 
 <br>
 This demo app is very very basic and is using 
